6178091b2ce78250df530abf9ec48b95cf09dde9
kzdfg23 <144627954+kzdfg23@users.noreply.github.com>
2026-02-11 00:41:17 +0000

    added conditions to recommendations

--- analyse_spend.py
+++ analyse_spend.py
@@ -2,13 +2,23 @@ import csv #import csv module
 
 with open('journeys.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file) #DictReader method to make field name keys for each value
+
     tot = 0
     for line in csv_reader:
         tot += float(line["fare"]) #Sum fare values
+
     print(f"Total PAYG spend: £{tot:.2f}")#print sum
+
     monthly_cost = 160
     print(f"Monthly travelcard cost: £{monthly_cost:.2f}")
+
     diff = monthly_cost - tot
-    print(f"Recommendation: PAYG is cheaper by £{diff:.2f}")#compare with monthly and give recommendation
+
+    if tot < monthly_cost:#compare with monthly and give recommendation
+        print(f"Recommendation: PAYG is cheaper by £{diff:.2f}")
+    elif tot > monthly_cost:
+        print(f"Recommendation: Travelcard is cheaper by £{abs(diff):.2f}")
+    else:
+        print("Recommendation: Both options cost the same.")
 
 
