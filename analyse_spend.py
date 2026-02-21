86d77e408cea1a10f580a0f37fb8f177609eef27
kzdfg23 <144627954+kzdfg23@users.noreply.github.com>
2026-02-21 02:01:17 +0000

    added my december csv got anaylsis

--- analyse_spend.py
+++ analyse_spend.py
@@ -1,15 +1,16 @@
 import csv #import csv module
 
-with open('journeys.csv', 'r') as csv_file:
+with open('December-journey.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file) #DictReader method to make field name keys for each value
 
     tot = 0
     for line in csv_reader:
-        tot += float(line["fare"]) #Sum fare values
+        #print(line)
+        tot += -(float(line["Daily Charge (GBP)"])) #Sum fare values
 
     print(f"Total PAYG spend: £{tot:.2f}")#print sum
 
-    monthly_cost = 160
+    monthly_cost = 172.50
     print(f"Monthly travelcard cost: £{monthly_cost:.2f}")
 
     diff = monthly_cost - tot
